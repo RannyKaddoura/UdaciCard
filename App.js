11d5172494c8a586a744d52568e29cb001dcc964
Ranny <ranny.kaddoura@starticket.ch>
2019-04-14 14:47:24 +0200

    add react-navigation and tabs

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TabNavigator from './Tabs'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>UdaciCards Application by Kaddoura</Text>
+        <TabNavigator />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/DeckList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class DeckList extends React.Component {
+  render() {
+    return <Text>DeckList</Text>;
+  }
+}
