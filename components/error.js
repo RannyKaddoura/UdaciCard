ebc54a568b9608fbac14cd3453fa00eecccce316
Ranny <ranny.kaddoura@starticket.ch>
2019-04-21 11:21:31 +0200

    error are now in red

--- components/error.js
+++ components/error.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { red, blue, green } from '../Colors';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { red, white } from '../Colors';
+import { Text, View, StyleSheet, StatusBar } from 'react-native';
+import { Constants } from 'expo'
+
 
 export default class Errors extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.container}>
+        <ErrorStatusBar backgroundColor={red} barStyle="light-content" />
         <Text style={[styles.input, { borderColor: red }]}>
           You cannot create Deck without title
         </Text>
@@ -14,13 +17,28 @@ export default class Errors extends React.Component {
   }
 }
 
+function ErrorStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
-    input: {
-      marginBottom: 40,
-      height: 50,
-      width: 300,
-      fontSize: 18,
-      borderColor: '#c6c7c8'
-    }
-  });
-  
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: red,
+    alignItems: 'center'
+  },
+  input: {
+    marginBottom: 40,
+    height: 100,
+    width: 300,
+    fontSize: 32,
+    color: white,
+    textAlign: 'center',
+    borderColor: '#c6c7c8'
+  }
+});
